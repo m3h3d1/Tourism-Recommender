65549dbcd48846320686b204bff40a821c7944e4
m3h3d1 <hasanmehedi185@gmail.com>
2021-09-27 13:13:02 +0600

    view

--- travelrecommend.sql
+++ travelrecommend.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 25, 2021 at 06:20 AM
+-- Generation Time: Sep 27, 2021 at 09:05 AM
 -- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.34
+-- PHP Version: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,75 +24,100 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `images`
+-- Table structure for table `places`
 --
 
-CREATE TABLE `images` (
-  `id` int(11) NOT NULL,
-  `image` varchar(100) NOT NULL,
-  `image_text` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `places` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `place_name` varchar(40) NOT NULL,
+  `division` varchar(20) NOT NULL,
+  `description` text NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `images`
+-- Dumping data for table `places`
 --
 
-INSERT INTO `images` (`id`, `image`, `image_text`) VALUES
-(1, '2.jpg', 'fh'),
-(2, 'back.jpg', 'view ');
+INSERT INTO `places` (`id`, `place_name`, `division`, `description`, `image`) VALUES
+(14, 'Saint Martin', 'Chittagong', 'Saint Martin is one of the most attractive and popular tourist place in Bangladesh', '01capture-1632648682.png'),
+(15, 'Coxs Bazar', 'Chattogram', 'Largest seabeach', '20171228043135660775-1632671534.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `registration`
+-- Table structure for table `rating`
 --
 
-CREATE TABLE `registration` (
-  `ID` int(11) NOT NULL,
-  `Name` varchar(56) NOT NULL,
-  `Email` varchar(256) NOT NULL,
-  `Password` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `rating` (
+  `place_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `rating` int(11) NOT NULL,
+  `review` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `registration`
+-- Dumping data for table `rating`
 --
 
-INSERT INTO `registration` (`ID`, `Name`, `Email`, `Password`) VALUES
-(1, 'priya', 'priyankapriya357@yahoo.com', '1234'),
-(2, 'arni', 'arni12@gmail.com', '12345');
+INSERT INTO `rating` (`place_id`, `user_id`, `rating`, `review`) VALUES
+(0, 0, 3, '32'),
+(0, 0, 0, '32'),
+(0, 0, 0, '22'),
+(0, 0, 0, '22'),
+(0, 0, 0, '13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `password`) VALUES
+(1, 'a', 'a@gmail.com', 'a');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `images`
+-- Indexes for table `places`
 --
-ALTER TABLE `images`
+ALTER TABLE `places`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `registration`
+-- Indexes for table `user`
 --
-ALTER TABLE `registration`
-  ADD PRIMARY KEY (`ID`);
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `images`
+-- AUTO_INCREMENT for table `places`
 --
-ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `places`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT for table `registration`
+-- AUTO_INCREMENT for table `user`
 --
-ALTER TABLE `registration`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `user`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- view_temp.php
+++ view_temp.php
@@ -3,33 +3,16 @@
  <title>display image</title>
  </head>
  <body>
- <p>Here in your form and text</p>
-   <?php
- 
-  // echo "<img src='upload/01capture-1632648682.png' height='500'/>"; 
-
-   ?>  
-   <?php
-  include_once 'database.php';
-  $image_details  = mysqli_query($con, "SELECT image FROM places where image='01capture-1632648682.png'");
-     while ($row = mysqli_fetch_array($image_details)) {     
-    
-        echo "<img src='upload/".$row['image']."' height='500'/>";   
-      
-    }     
-
-  ?>
-  <br>
-
+  
   <?php 
-  // Check to see the URL variable is set and that it exists in the database
+ /* // Check to see the URL variable is set and that it exists in the database
   if (isset($_GET['id'])) {
     // Connect to the MySQL database  
       include "database.php"; 
     $id = preg_replace('#[^0-9]#i', '', $_GET['id']); 
     // Use this var to check to see if this ID exists, if yes then get the product 
     // details, if no then exit this script and give message why
-    $sql = mysql_query("SELECT * FROM products WHERE id='$id' LIMIT 1");
+    $sql = mysql_query($con, "SELECT place_name FROM places WHERE id='$id' LIMIT 1");
     $productCount = mysql_num_rows($sql); // count the output amount
       if ($productCount > 0) {
       // get all the product details
@@ -50,7 +33,44 @@
     echo "Data to render this page is missing.";
     exit();
   }
-  mysql_close();
+  mysql_close();*/
+  ?>
+
+
+   <?php
+ 
+  // echo "<img src='upload/01capture-1632648682.png' height='500'/>"; 
+
+   ?>  
+   <?php
+      
+    if (isset($_GET['division'])) {
+      // Connect to the MySQL database  
+        include "database.php"; 
+      // $division = preg_replace('#[^0-9]#i', '', $_GET['division']); 
+      $division = $_GET['division']; 
+      // Use this var to check to see if this division exists, if yes then get the product 
+      // details, if no then exit this script and give message why
+      echo "$division\n";
+      $sql  = mysqli_query($con, "SELECT * FROM places where division='$division' LIMIT 1");
+      $cnt = mysqli_num_rows($sql); // count the output amount
+      if ($cnt > 0) {
+           while ($row = mysqli_fetch_array($sql)) {     
+          
+              echo "<img src='upload/".$row['image']."' height='500'/>";   
+              echo "<br>Place Name = {$row['place_name']} <br>";
+              echo "Division = {$row['division']} <br>";
+              echo "Description: {$row['description']} <br>";
+          }
+      } else {
+        echo "That item does not exist.";
+          exit();
+      }
+    }
+    else {
+        echo "Data to render this page is missing.";
+        exit();
+    }
   ?>
  </body>
  </html>
